4ce1d03e1d4e8085117c0bf0cd74b5c009a586f0
AndreJr117 <andre_jr97@hotmail.com>
2023-04-09 18:46:42 -0300

    Start Commit!

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setUpView()
+
+    }
+
+    fun handleOnClickEvent(view: View){
+        Toast.makeText(this,"OnClickEvent",
+        Toast.LENGTH_SHORT).show()
+    }
+
+    private fun setUpView() {
+
+        binding.button2.setOnClickListener {
+            val email = binding.textView2.text.toString()
+            val senha = binding.textView3.text.toString()
+
+            if (email.equals("aluno@nassau.com.br") && senha.equals("alunonassau")) {
+                Toast.makeText(this, "Logado com sucesso!", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Email ou senha invalido", Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+    }
+
+}
